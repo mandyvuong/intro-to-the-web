2b6516f248c588f362b5d339e02ef5ba9a452cc2
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-06 20:50:53 +0100

    Sinatra: erb

--- app.rb
+++ app.rb
@@ -2,7 +2,5 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/cat' do
-  "<body>
-     <img style='border: dashed red' src='https://i.imgur.com/jFaSxym.png'>
-   </body>"
+  erb(:index)
 end
